9610d58295ecdc8911abd2dbe83559611fa7761a
Isaac Elliott <isaace71295@gmail.com>
2025-10-04 11:24:41 +1000

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 module Main where
 
-import Control.Applicative ((<**>), (<|>))
+import Control.Applicative ((<**>), (<|>), some, optional, Const (..))
 import qualified Options.Applicative as Options
 import qualified Todo
 import qualified System.Environment
 import Data.Time.Clock (getCurrentTime)
 import qualified System.Process as Process
-import System.Directory (doesFileExist, renameFile, removeFile, createDirectoryIfMissing)
+import System.Directory (doesFileExist, renameFile, removeFile, createDirectoryIfMissing, copyFile)
 import System.Exit (ExitCode(..), exitFailure)
 import Data.Time.Format (formatTime, defaultTimeLocale)
 import Data.Functor.Identity (Identity, runIdentity)
@@ -18,14 +19,23 @@ import Data.Text (Text)
 import qualified Data.Text.Lazy.IO as LazyText
 import qualified Data.Text as Text
 import Prelude hiding (init)
-import Data.Foldable (traverse_)
+import Data.Foldable (traverse_, foldl', foldlM, for_)
 import qualified Data.Map as Map
-import Data.List (sortOn)
+import Data.List (sortOn, intercalate)
 import qualified Data.Text.IO as Text.IO
 import Data.Map (Map)
 import Data.Monoid (All(..))
-import Barbies (bfoldMap)
+import Barbies (bfoldMap, bzipWith, btraverseC, bzip)
 import Data.Ord (Down(..))
+import Data.String (fromString)
+import qualified Data.Char as Char
+import Data.Attoparsec.Combinator (lookAhead)
+import Control.Exception (finally)
+import Data.Functor.Compose (Compose (..))
+import Control.Monad (when, unless)
+import Data.Set (Set)
+import Data.Functor.Product (Product(..))
+import Data.Functor (void)
 
 data Cli
   = Cli{ database :: FilePath, command :: Command }
@@ -112,8 +122,7 @@ default_ path = do
         else putStr "  "
       case Map.toList (Todo.title task) of
         [] -> undefined
-        [(_stateId, title)] -> Text.IO.putStrLn title
-        _titles -> error "TODO: title conflict"
+        (_stateId, title) : _ -> Text.IO.putStrLn title
 
 init :: FilePath -> IO ()
 init path = do
@@ -152,9 +161,10 @@ newTask path = do
       exists <- doesFileExist taskFile
       if exists
         then do
-          task <- readTask taskFile
+          task  <- readTask taskFile resolvedTaskParser
           state <- Todo.stateDeserialise path
           state' <- Todo.stateChange (Todo.NewTask task) state
+
           let pathNew = path ++ ".new"
           Todo.stateSerialise pathNew state'
           renameFile pathNew path
@@ -164,32 +174,256 @@ newTask path = do
           putStrLn $ "Created task " ++ Text.unpack (runIdentity $ Todo.title task)
         else putStrLn "Aborted"
 
-readTask :: FilePath -> IO (Todo.Task Identity)
-readTask path = do
+readTask :: FilePath -> Attoparsec.Parser a -> IO a
+readTask path parser = do
   input <- LazyText.readFile path
-  case Attoparsec.parseOnly taskParser input of
+  case Attoparsec.parseOnly (parser <* Attoparsec.endOfInput) input of
     Left{} ->
       error $ "failed to parse task " ++ path
-    Right (title, description) ->
-      pure Todo.Task{ Todo.title = pure title, Todo.description = pure description }
-  where
-    taskParser :: Attoparsec.Parser (Text, Text)
-    taskParser =
-      (,) <$>
-      Attoparsec.takeWhile (/= '\n') <*>
-      (Attoparsec.takeWhile1 (== '\n') *> Attoparsec.takeWhile (const True) <|> pure Text.empty)
+    Right task ->
+      pure task
+
+fieldCommentParser :: String -> Attoparsec.Parser (Maybe Todo.StateId)
+fieldCommentParser field =
+  Attoparsec.char '!' *>
+  Attoparsec.takeWhile (\c -> Char.isSpace c && c /= '\n') *>
+  Attoparsec.string (fromString field) *>
+  Attoparsec.takeWhile (\c -> Char.isSpace c && c /= '\n') *>
+  optional (
+    Attoparsec.char '(' *>
+    (do
+      chars <- Attoparsec.takeWhile1 Char.isAlphaNum
+      case Todo.md5FromBase32 $ Text.unpack chars of
+        Nothing -> fail "invalid state ID"
+        Just md5 -> pure $ Todo.StateId md5
+    ) <*
+    Attoparsec.char ')' <*
+    Attoparsec.takeWhile (\c -> Char.isSpace c && c /= '\n')
+  ) <*
+  Attoparsec.char '\n'
+
+titleParser :: Attoparsec.Parser Text
+titleParser = Attoparsec.takeWhile (/= '\n') <* Attoparsec.takeWhile Char.isSpace
+
+data Identified a
+  = Unidentified a
+  | Identified Todo.StateId a
+  deriving (Show, Eq)
+
+data ValidateIdentifiedError
+  -- | The change proposes multiple distinct values for a field.
+  --
+  -- Example:
+  --
+  -- @
+  -- ! title
+  -- Title one
+  --
+  -- ! title
+  -- Title two
+  -- @
+  --
+  -- A change should not conflict with itself. 
+  = ConflictingResolutions
+      -- | Field name
+      !Text
+
+  -- | The change proposes a new value for a field in addition to a conflict set.
+  --
+  -- Example:
+  -- 
+  -- @
+  -- ! title (ID)
+  -- Title one
+  --
+  -- ! title
+  -- Title two
+  -- @
+  --
+  -- A new value should be proposed, or conflicts should be proposed, but not both.
+  | UnexpectedResolution
+      -- | Field name
+      !Text
+      -- | Current proposed conflicts
+      !(Set Todo.StateId)
+
+  -- | The change proposes a conflict set in addition to a new value.
+  --
+  -- Example:
+  -- 
+  -- @
+  -- ! title
+  -- Title two
+  --
+  -- ! title (ID)
+  -- Title one
+  -- @
+  --
+  -- A new value should be proposed, or conflicts should be proposed, but not both.
+  | UnexpectedConflict
+      -- | Field name
+      !Text
+      -- | Proposed conflict
+      !Todo.StateId
+
+  -- | A field has different values proposed for the same 'StateId'.
+  --
+  -- Example:
+  --
+  -- @
+  -- ! title (ID)
+  -- Title one
+  --
+  -- ! title (ID)
+  -- Title two
+  -- @
+  --
+  -- A change should not conflict with itself.
+  | InconsistentConflict
+      -- | Field name
+      !Text
+      !Todo.StateId
+
+  -- | The proposed field change is empty.
+  | EmptyChange
+      -- | Field name
+      !Text
+  deriving (Show, Eq)
+
+validateIdentifiedTask :: Todo.Task (Compose [] Identified) -> Either ValidateIdentifiedError (Todo.Task Todo.Conflicted)
+validateIdentifiedTask =
+  btraverseC @Eq
+    (\(Const fieldName `Pair` Compose identifieds) -> do
+      let
+        f Nothing (Unidentified a) =
+          pure . Just $ Todo.Resolved a
+        f acc@(Just (Todo.Resolved a)) (Unidentified a') = do
+          when (a /= a') . Left $ ConflictingResolutions fieldName
+          pure acc
+        f acc@(Just (Todo.Conflicted as)) (Unidentified a') = do
+          unless (a' `elem` as) . Left $ UnexpectedResolution fieldName (Map.keysSet as)
+          pure acc
+        f Nothing (Identified stateId a) =
+          pure . Just $ Todo.Conflicted (Map.singleton stateId a)
+        f (Just (Todo.Resolved a)) (Identified stateId a') = do
+          when (a /= a') . Left $ UnexpectedConflict fieldName stateId
+          pure . Just $ Todo.Conflicted (Map.singleton stateId a')
+        f (Just (Todo.Conflicted as)) (Identified stateId a') = do
+          for_ (Map.lookup stateId as) $ \a ->
+            when (a /= a') $ Left $ InconsistentConflict fieldName stateId
+          pure . Just . Todo.Conflicted $ Map.insert stateId a' as 
+
+      result <- foldlM f Nothing identifieds
+      case result of
+        Just conflicted -> pure conflicted
+        Nothing -> Left $ EmptyChange fieldName
+    ) .
+    bzip Todo.taskFieldNames
+
+identifiedTaskParser :: Attoparsec.Parser (Todo.Task (Compose [] Identified))
+identifiedTaskParser =
+  foldl' (bzipWith (\(Compose a) (Compose b) -> Compose (a ++ b))) Todo.Task{ Todo.title = mempty, Todo.description = mempty } <$>
+  some
+    ((do
+        mStateId <- fieldCommentParser "title"
+        title <- titleParser
+        case mStateId of
+          Nothing ->
+            pure Todo.Task{ Todo.title = Compose [Unidentified title], Todo.description = mempty }
+          Just stateId ->
+            pure Todo.Task{ Todo.title = Compose [Identified stateId title], Todo.description = mempty }
+      ) <|>
+      (do
+        mStateId <- fieldCommentParser "description"
+        description <-
+          fmap
+            Text.pack
+            (Attoparsec.manyTill
+              Attoparsec.anyChar
+              (void (lookAhead . Attoparsec.string $ fromString "\n!") <|> Attoparsec.endOfInput) <*
+              optional (Attoparsec.char '\n')
+            )
+        case mStateId of
+          Nothing ->
+            pure Todo.Task{ Todo.title = mempty, Todo.description = Compose [Unidentified description] }
+          Just stateId ->
+            pure Todo.Task{ Todo.title = mempty, Todo.description = Compose [Identified stateId description] }
+      )
+    )
+
+resolvedTaskParser :: Attoparsec.Parser (Todo.Task Identity)
+resolvedTaskParser = do
+  title <- titleParser
+  description <- Attoparsec.takeWhile (const True)
+  pure Todo.Task{ Todo.title = pure title, Todo.description = pure description }
 
 writeTask :: FilePath -> Todo.Task (Map Todo.StateId) -> IO ()
 writeTask path task =
-  writeFile path $
+  writeFile path . intercalate "\n\n" $
   (case Map.toList (Todo.title task) of
-    [(_, title)] -> Text.unpack title ++ "\n\n"
-    _ -> error "TODO: print title conflict"
+    [(_, title)] ->
+      [ (if hasConflicts then "! title\n" else "") ++
+        Text.unpack title
+      ]
+    titles ->
+      fmap
+        (\(stateId, title) -> "! title (" ++ Todo.renderStateId stateId ++ ")\n" ++ Text.unpack title)
+        titles
   ) ++
   (case Map.toList (Todo.description task) of
-    [(_, description)] -> Text.unpack description
-    _ -> error "TODO: print description conflict"
+    [(_, description)] ->
+      [ (if hasConflicts then "! description\n" else "") ++
+        Text.unpack description
+      ]
+    descriptions ->
+      fmap
+        (\(stateId, description) -> "! description (" ++ Todo.renderStateId stateId ++ ")\n" ++ Text.unpack description)
+        descriptions
   )
+  where
+    hasConflicts =
+      Map.size (Todo.title task) > 1 ||
+      Map.size (Todo.description task) > 1
+
+renderUpdateTask :: Todo.Task (Map Todo.StateId) -> Todo.Task Todo.Update -> String
+renderUpdateTask task update =
+  foldMap (++ "\n\n") $
+  renderTextField "title" Todo.title ++
+  renderTextField "description" Todo.description
+  where
+    prependLines prefix =
+      intercalate "\n" . fmap ((prefix ++) . Text.unpack) . Text.splitOn (fromString "\n")
+
+    renderTextField :: String -> (forall f. Todo.Task f -> f Text) -> [String]
+    renderTextField fieldName getter =
+      case getter update of
+        Todo.None ->
+          []
+        Todo.Set a ->
+          foldMap
+            (\(stateId', a') ->
+              [ "- ! " ++ fieldName ++ " (" ++ Todo.renderStateId stateId' ++ ")\n" ++
+                prependLines "- " a'
+              ]
+            )
+            (Map.toList $ getter task) ++
+          [ "+ ! " ++ fieldName ++ "\n" ++
+            prependLines "+ " a
+          ]
+        Todo.Pick stateId ->
+          foldMap
+            (\(stateId', a') ->
+              if stateId == stateId'
+              then
+                [ "  ! " ++ fieldName ++ " (" ++ Todo.renderStateId stateId' ++ ")\n" ++
+                  prependLines "  " a'
+                ]
+              else
+                [ "- ! " ++ fieldName ++ " (" ++ Todo.renderStateId stateId' ++ ")\n" ++
+                  prependLines "- " a'
+                ]
+            )
+            (Map.toList $ getter task)
 
 editTask :: FilePath -> Todo.TaskId -> IO ()
 editTask path taskId = do
@@ -204,30 +438,52 @@ editTask path taskId = do
 
       now <- getCurrentTime
 
-      let taskFile = "drafts/" ++ Todo.gidToBase32 (Todo.unTaskId taskId) ++ formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ" now ++ ".txt"
-      writeTask taskFile task
+      let taskFile = "drafts/" ++ formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ" now ++ "-" ++ Todo.gidToBase32 (Todo.unTaskId taskId) ++ ".txt"
+      let taskFileBase = taskFile ++ ".base"
+      writeTask taskFileBase task
+      copyFile taskFileBase taskFile
 
-      exitCode <- Process.withCreateProcess (Process.proc editor [taskFile]) $ \_mStdin _mStdout _mStderr processHandle -> do
-        Process.waitForProcess processHandle
-      case exitCode of
-        ExitFailure status -> do
-          putStrLn $ "Aborted (editor exited with status " ++ show status ++ ")"
-          exitFailure
-        ExitSuccess -> do
-          exists <- doesFileExist taskFile
-          if exists
-            then do
-              task' <- readTask taskFile
-              let updateTask = Todo.taskDiff task task'
+      (do
+        exitCode <- Process.withCreateProcess (Process.proc editor [taskFile]) $ \_mStdin _mStdout _mStderr processHandle -> do
+          Process.waitForProcess processHandle
+        case exitCode of
+          ExitFailure status -> do
+            putStrLn $ "Aborted (editor exited with status " ++ show status ++ ")"
+            exitFailure
+          ExitSuccess -> do
+            base <- readFile taskFileBase
+            new <- readFile taskFile
+            if base == new
+              then putStrLn "No change"
+              else do
+                result <- readTask taskFile (Left <$> identifiedTaskParser <|> Right <$> resolvedTaskParser)
+                updateTask <-
+                  case result of
+                    Left identifiedTask -> do
+                      conflictedTask <-
+                        -- TODO: nicer error message
+                        either (\err -> print err *> exitFailure) pure $
+                        validateIdentifiedTask identifiedTask
+                      case Todo.taskDiffConflicted task conflictedTask of
+                        Right updateTask -> pure updateTask
+                        Left err -> do
+                          -- TODO: nicer error message
+                          print err
+                          exitFailure
+                    Right task' -> do
+                      pure $ Todo.taskDiff task task'
 
-              if getAll $ bfoldMap (All . (\case; Todo.None -> True; _ -> False)) updateTask
-                then putStrLn "No change"
-                else do
-                  state' <- Todo.stateChange Todo.UpdateTask{ Todo.taskId, Todo.updateTask } state
-                  let pathNew = path ++ ".new"
-                  Todo.stateSerialise pathNew state'
-                  renameFile pathNew path
-                  putStrLn $ "Updated task " ++ Todo.gidToBase32 (Todo.unTaskId taskId)
+                if getAll $ bfoldMap (All . (\case; Todo.None -> True; _ -> False)) updateTask
+                  then putStrLn "No change"
+                  else do
+                    putStrLn "Changes:\n"
 
-              removeFile taskFile
-            else putStrLn "Aborted"
+                    putStr $ renderUpdateTask task updateTask
+                    state' <- Todo.stateChange Todo.UpdateTask{ Todo.taskId, Todo.updateTask } state
+                    let pathNew = path ++ ".new"
+                    Todo.stateSerialise pathNew state'
+                    renameFile pathNew path
+                    removeFile taskFile
+                    putStrLn $ "Updated task " ++ Todo.gidToBase32 (Todo.unTaskId taskId)
+        ) `finally`
+          removeFile taskFileBase
