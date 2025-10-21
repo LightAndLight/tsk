595230dc5e19f45d7eb97a20043fd0b0f922ec31
Isaac Elliott <isaace71295@gmail.com>
2025-10-22 07:29:31 +1000

    Add `comment new` command

--- src/Todo/Comment.hs
+++ src/Todo/Comment.hs
@@ -16,9 +16,9 @@ import Data.String (fromString)
 import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
 import GHC.Generics (Generic)
-import GID (GID, gidToBase32, newGID)
+import GID (GID, gidFromBase32, gidToBase32, newGID)
 import Getter (Getter (..))
-import Todo.Task (TaskId, renderTaskId)
+import Todo.Task (TaskId, parseTaskId, renderTaskId)
 
 newtype CommentId = CommentId {unCommentId :: GID}
   deriving (Show, Eq, Ord)
@@ -26,6 +26,9 @@ newtype CommentId = CommentId {unCommentId :: GID}
 
 renderCommentId :: CommentId -> String
 renderCommentId (CommentId gid) = gidToBase32 gid
+
+parseCommentId :: String -> Maybe CommentId
+parseCommentId = fmap CommentId . gidFromBase32
 
 newCommentId :: IO CommentId
 newCommentId = CommentId <$> newGID
@@ -39,6 +42,19 @@ renderReplyId :: ReplyId -> String
 renderReplyId (ReplyTask taskId) = "task:" ++ renderTaskId taskId
 renderReplyId (ReplyComment commentId) = "comment:" ++ renderCommentId commentId
 
+parseReplyId :: String -> Maybe ReplyId
+parseReplyId input =
+  let
+    (prefix, suffix) = break (== ':') input
+  in
+    case prefix of
+      "task" ->
+        ReplyTask <$> parseTaskId (drop 1 suffix)
+      "comment" ->
+        ReplyComment <$> parseCommentId (drop 1 suffix)
+      _ ->
+        Nothing
+
 data CommentMetadata
   = CommentMetadata
   { createdAt :: !UTCTime
@@ -46,11 +62,12 @@ data CommentMetadata
   }
   deriving (Show, Eq)
 
-data Comment f
+newtype Comment f
   = Comment
-  { description :: !(f Text)
+  { description :: f Text
   }
-  deriving (Generic, FunctorB, TraversableB, ApplicativeB, ConstraintsB, DistributiveB)
+  deriving (Generic)
+  deriving anyclass (FunctorB, TraversableB, ApplicativeB, ConstraintsB, DistributiveB)
 
 deriving instance (forall x. Show x => Show (f x)) => Show (Comment f)
 deriving instance (forall x. Eq x => Eq (f x)) => Eq (Comment f)
